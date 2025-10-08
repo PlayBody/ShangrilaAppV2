343d5878c06f7884ff9836df8084ddd45916f9a7
Playbody <playbody2021@gmail.com>
2025-10-07 22:24:46 -0400

    f

--- sapp/lib/src/common/apiendpoint.dart
+++ sapp/lib/src/common/apiendpoint.dart
@@ -2,72 +2,72 @@ String apiBase = 'https://silverfeathers.net';
 String apiNewBase = 'https://silverfeathers.net/apis';
 //String apiBase = 'http://192.168.145.209/cloud_devotion';
 
-String organImageUrl = apiBase + '/assets/images/organs/';
-String ticketImageUrl = apiBase + '/assets/images/tickets/';
-String menuImageUrl = apiBase + '/assets/images/menus/';
-String adviseMovieBase = apiBase + '/assets/video/advise/';
+String organImageUrl = '$apiBase/assets/images/organs/';
+String ticketImageUrl = '$apiBase/assets/images/tickets/';
+String menuImageUrl = '$apiBase/assets/images/menus/';
+String adviseMovieBase = '$apiBase/assets/video/advise/';
 
-String apiLoadCompanyStaffListUrl = apiBase + '/apistaffs/loadStaffCompanyList';
-String apiAddStaffPointDataUrl = apiBase + '/apistaffs/submitAddPoint';
+String apiLoadCompanyStaffListUrl = '$apiBase/apistaffs/loadStaffCompanyList';
+String apiAddStaffPointDataUrl = '$apiBase/apistaffs/submitAddPoint';
 
-String apiLoadUserFromQrCodeUrl = apiBase + '/apiusers/loadUserFromQrNo';
-String apiSaveUserInfoUrl = apiBase + '/apiusers/saveUserInfo';
+String apiLoadUserFromQrCodeUrl = '$apiBase/apiusers/loadUserFromQrNo';
+String apiSaveUserInfoUrl = '$apiBase/apiusers/saveUserInfo';
 String apiDeleteUserInfoUrl = '$apiBase/apiusers/deleteUser';
 
-String apiLoadOrganListUrl = apiBase + '/apiorgans/loadOrganList';
-String apiLoadOrganInfo = apiBase + '/apiorgans/loadOrganInfo';
-String apiSaveOrganUrl = apiBase + '/apiorgans/saveOrgan';
-String apiDeleteOrgan = apiBase + '/apiorgans/deleteOrgan';
-String apiUploadOrganPhoto = apiBase + '/apiorgans/uploadPicture';
+String apiLoadOrganListUrl = '$apiBase/apiorgans/loadOrganList';
+String apiLoadOrganInfo = '$apiBase/apiorgans/loadOrganInfo';
+String apiSaveOrganUrl = '$apiBase/apiorgans/saveOrgan';
+String apiDeleteOrgan = '$apiBase/apiorgans/deleteOrgan';
+String apiUploadOrganPhoto = '$apiBase/apiorgans/uploadPicture';
 
-String apiLoadMenuViewList = apiBase + '/apimenus/loadViewMenuList';
-String apiLoadAdminMenuInfoUrl = apiBase + '/apimenus/loadAdminMenuInfo';
-String apiLoadMenuListUrl = apiBase + '/apimenus/loadMenuList';
+String apiLoadMenuViewList = '$apiBase/apimenus/loadViewMenuList';
+String apiLoadAdminMenuInfoUrl = '$apiBase/apimenus/loadAdminMenuInfo';
+String apiLoadMenuListUrl = '$apiBase/apimenus/loadMenuList';
 
-String apiLoadMessagesUrl = apiBase + '/apimessages/loadMessages';
-String apiLoadMessageUserListUrl = apiBase + '/apimessages/loadMessageUserList';
+String apiLoadMessagesUrl = '$apiBase/apimessages/loadMessages';
+String apiLoadMessageUserListUrl = '$apiBase/apimessages/loadMessageUserList';
 String apiUploadMessageAttachFileUrl =
-    apiBase + '/apimessages/uploadAttachment';
+    '$apiBase/apimessages/uploadAttachment';
 
-String apiLoadCouponListUrl = apiBase + '/apicoupons/loadCouponList';
-String apiLoadUserCouponsUrl = apiBase + '/apicoupons/loadUserCouponList';
+String apiLoadCouponListUrl = '$apiBase/apicoupons/loadCouponList';
+String apiLoadUserCouponsUrl = '$apiBase/apicoupons/loadUserCouponList';
 String apiUpdateUserCouponUseFlagUrl =
-    apiBase + '/apicoupons/updateUserCouponUseFlag';
-String apiDeleteUserCouponUrl = apiBase + '/apicoupons/deleteUserCoupon';
-String apiDeleteCouponInfoUrl = apiBase + '/apicoupons/deleteCouponInfo';
-String apiLoadCouponInfoUrl = apiBase + '/apicoupons/getUserCouponInfo';
-String apiSaveCouponUrl = apiBase + '/apicoupons/saveCoupon';
-String apiLoadUserStampUrl = apiBase + '/apicoupons/loadUserStampList';
-String apiLoadDayStampCntUrl = apiBase + '/apicoupons/loadDayStampCount';
+    '$apiBase/apicoupons/updateUserCouponUseFlag';
+String apiDeleteUserCouponUrl = '$apiBase/apicoupons/deleteUserCoupon';
+String apiDeleteCouponInfoUrl = '$apiBase/apicoupons/deleteCouponInfo';
+String apiLoadCouponInfoUrl = '$apiBase/apicoupons/getUserCouponInfo';
+String apiSaveCouponUrl = '$apiBase/apicoupons/saveCoupon';
+String apiLoadUserStampUrl = '$apiBase/apicoupons/loadUserStampList';
+String apiLoadDayStampCntUrl = '$apiBase/apicoupons/loadDayStampCount';
 
 String apiLoadFavortieQuestionUrl =
-    apiBase + '/apiquestions/loadFavoriteQuestions';
+    '$apiBase/apiquestions/loadFavoriteQuestions';
 String apiSaveFavortieQuestionUrl =
-    apiBase + '/apiquestions/saveFavoriteQuestion';
+    '$apiBase/apiquestions/saveFavoriteQuestion';
 
-String apiSaveQuestionUrl = apiBase + '/apiquestions/saveQuestion';
+String apiSaveQuestionUrl = '$apiBase/apiquestions/saveQuestion';
 
-String apiLoadReserveDataUrl = apiBase + '/apireserves/loadUserReserveData';
-String apiLoadReserveSelStatusUrl = apiBase + '/apireserves/loadSelectStatus';
-String apiLoadReservePossibleUrl = apiBase + '/apireserves/loadPossibleReserve';
-String apiSaveReserveDataUrl = apiBase + '/apireserves/saveUserReserve';
-String apiLoadReserveInfo = apiBase + '/apireserves/loadReserveInfo';
+String apiLoadReserveDataUrl = '$apiBase/apireserves/loadUserReserveData';
+String apiLoadReserveSelStatusUrl = '$apiBase/apireserves/loadSelectStatus';
+String apiLoadReservePossibleUrl = '$apiBase/apireserves/loadPossibleReserve';
+String apiSaveReserveDataUrl = '$apiBase/apireserves/saveUserReserve';
+String apiLoadReserveInfo = '$apiBase/apireserves/loadReserveInfo';
 
-String apiRegisterDeviceToekn = apiBase + '/api/registerDeviceToken';
-String apiLoadSaleSiteUrl = apiBase + '/api/loadSaleSite';
+String apiRegisterDeviceToekn = '$apiBase/api/registerDeviceToken';
+String apiLoadSaleSiteUrl = '$apiBase/api/loadSaleSite';
 
-String apiLoadTeacherListUrl = apiBase + '/apiteachers/loadTeacherList';
+String apiLoadTeacherListUrl = '$apiBase/apiteachers/loadTeacherList';
 
-String apiLoadAdviseListUrl = apiBase + '/apiadvises/loadAdviseList';
-String apiLoadAdviseInfoUrl = apiBase + '/apiadvises/loadAdviseInfo';
-String apiSaveAdviseInfoUrl = apiBase + '/apiadvises/saveAdviseInfo';
-String apiUploadAdviseVideo = apiBase + '/apiadvises/uploadVideo';
+String apiLoadAdviseListUrl = '$apiBase/apiadvises/loadAdviseList';
+String apiLoadAdviseInfoUrl = '$apiBase/apiadvises/loadAdviseInfo';
+String apiSaveAdviseInfoUrl = '$apiBase/apiadvises/saveAdviseInfo';
+String apiUploadAdviseVideo = '$apiBase/apiadvises/uploadVideo';
 
-String apiLoadMenuReviewUrl = apiBase + '/apireviews/loadMenuReview';
-String apiSaveMenuReviewUrl = apiBase + '/apireviews/saveMenuReview';
+String apiLoadMenuReviewUrl = '$apiBase/apireviews/loadMenuReview';
+String apiSaveMenuReviewUrl = '$apiBase/apireviews/saveMenuReview';
 
-String apiLoadCardListUrl = apiBase + '/apisquare/loadCardList';
-String apiRenderPrintLogo = apiBase + '/apiorgans/renderPrintLogo?organ_id=';
+String apiLoadCardListUrl = '$apiBase/apisquare/loadCardList';
+String apiRenderPrintLogo = '$apiBase/apiorgans/renderPrintLogo?organ_id=';
 
-String floorImageUrl = apiBase + '/assets/images/floors/';
-String apiNewSaleUrl = apiNewBase + '/sales/isNewSaleNews';
+String floorImageUrl = '$apiBase/assets/images/floors/';
+String apiNewSaleUrl = '$apiNewBase/sales/isNewSaleNews';
